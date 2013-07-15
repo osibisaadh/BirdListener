a40e1115aaaf48f1c2c7637b9b7ebf21d20006c5
osibisaadh <ohardy@student.neumont.edu>
2013-07-14 21:09:41 -0600

    Tasks updated based on new research Driver updated with tests, trying to figure out what data means. Test Class contains more experiments with wave data

--- Code/Spectrogram/src/Driver.java
+++ Code/Spectrogram/src/Driver.java
@@ -1,3 +1,8 @@
+import com.musicg.graphic.GraphicRender;
+import com.musicg.wave.Wave;
+import com.musicg.wave.extension.Spectrogram;
+
+
 /**
  * Created with IntelliJ IDEA.
  * User: Osibisaad
@@ -6,4 +11,49 @@
  * To change this template use File | Settings | File Templates.
  */
 public class Driver {
+
+    public static void main(String[] args) {
+        try{
+            Wave wave = new Wave("D:\\Downloads\\1.wav");
+            Spectrogram spectrogram = wave.getSpectrogram();
+            GraphicRender render = new GraphicRender();
+
+            double[][] data = spectrogram.getNormalizedSpectrogramData();
+            int count = 0;
+            double max = 0;
+            double min = 1;
+            int dlength = 0;
+            boolean dChanged = false;
+            boolean first = true;
+            for(double[] d : data){
+                if(first){
+                    dlength = d.length;
+                    first = false;
+                }
+                for(int i = 0; i < d.length; i++){
+                    System.out.print(d[i] + "--" );
+                    if(max < d[i])
+                        max = d[i];
+                    else if(min > d[i])
+                        min = d[i];
+                }
+                if(dlength < d.length){
+                    dChanged = true;
+                    dlength = d.length;
+                }
+                count++;
+                System.out.println("");
+            }
+
+            System.out.println("x = " + count);
+            System.out.println("y = " + dlength + " changed? " + dChanged);
+            System.out.println("Max: " + max + "\nMin: " + min );
+            render.setVerticalMarker(1000);
+            render.renderSpectrogramData(data, "D:\\11.jpg");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
 }
---
+++ Code/Spectrogram/src/Test.java
@@ -0,0 +1,84 @@
+import com.musicg.graphic.GraphicRender;
+import edu.emory.mathcs.jtransforms.fft.DoubleFFT_1D;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import java.io.File;
+import java.nio.ByteBuffer;
+
+public class Test {
+    public static void main(String[] args) {
+        try{
+            //Check out audio File info
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("D:\\Downloads\\1.wav"));
+            AudioFormat format = audioInputStream.getFormat();
+            System.out.println("Channels: " + format.getChannels());
+            System.out.println("Encoding: " + format.getEncoding());
+            System.out.println("FrameRate: " + format.getFrameRate());
+            System.out.println("Frame Size: " + format.getFrameSize());
+            System.out.println("SampleRate: " + format.getSampleRate());
+            System.out.println("BigEdian: " + format.isBigEndian());
+            System.out.println("Framlength: " + audioInputStream.getFrameLength());
+            System.out.println("available: " +audioInputStream.available());
+            //Create byte array for data
+            byte[] data = new  byte[audioInputStream.available()];
+
+
+            int bytesPerFrame =
+                    audioInputStream.getFormat().getFrameSize();
+            // Set buffer size of 1024 frames.
+            int numBytes = 1024 * bytesPerFrame;
+            byte[] audioBytes = new byte[numBytes];
+            int totalFramesRead =0;
+
+            int numBytesRead = 0;
+            int numFramesRead = 0;
+            // Try to read numBytes bytes from the file.
+            int count = 0;
+            while ((numBytesRead = audioInputStream.read(audioBytes)) != -1) {
+
+                // Calculate the number of frames actually read.
+                numFramesRead = numBytesRead / bytesPerFrame;
+                totalFramesRead += numFramesRead;
+                // Here, do something useful with the audio data that's
+                // now in the audioBytes array...
+                for(int i = 0; i < numBytesRead; i++){
+                    data[count++] = audioBytes[i];
+                }
+
+
+            }
+            double[] realdata = new double[data.length/8];
+            for(int i = 0; i < realdata.length; i++){
+                realdata[i] = toDouble(new byte[]{data[i*8],data[i*8 + 1],data[i*8 + 2],data[i*8 + 3], data[i*8 + 4],data[i*8 + 5], data[i*8 + 6],data[i*8 + 7]});
+            }
+            for(int i = 0; i< realdata.length; i++){
+                System.out.println(realdata[i]);
+            }
+
+            DoubleFFT_1D fft = new DoubleFFT_1D(2048);
+            fft.complexForward(realdata);
+
+            for(int i = 0; i< realdata.length; i++){
+                System.out.println("FFT " + i + ": " + realdata[i]);
+            }
+
+
+            System.out.println("Total bytes read: " + totalFramesRead * bytesPerFrame);
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static byte[] toByteArray(double value) {
+        byte[] bytes = new byte[8];
+        ByteBuffer.wrap(bytes).putDouble(value);
+        return bytes;
+    }
+
+    public static double toDouble(byte[] bytes) {
+        return ByteBuffer.wrap(bytes).getDouble();
+    }
+}
