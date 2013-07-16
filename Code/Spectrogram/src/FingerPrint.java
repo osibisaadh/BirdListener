b6228371287045aaf3804409c179efaa63c1c5ec
osibisaadh <ohardy@student.neumont.edu>
2013-07-16 15:04:37 -0600

    Updates to tasks and backlog as well as added testData folder to spectrogram project

---
+++ Code/Spectrogram/src/FingerPrint.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Osibisaad
+ * Date: 7/16/13
+ * Time: 2:14 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FingerPrint {
+}
