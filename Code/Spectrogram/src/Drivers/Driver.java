2fe5dfb99b066fbe17170eb210d25e0241517b61
osibisaadh <ohardy@student.neumont.edu>
2013-07-28 23:13:03 -0600

    Updates to testing main method classes

--- Code/Spectrogram/src/Drivers/Driver.java
+++ Code/Spectrogram/src/Drivers/Driver.java
@@ -1,8 +1,6 @@
 package Drivers;
 
 import com.musicg.graphic.GraphicRender;
-import com.musicg.wave.Wave;
-import com.musicg.wave.extension.Spectrogram;
 
 
 /**
@@ -16,45 +14,45 @@ public class Driver {
 
     public static void main(String[] args) {
         try{
-            //spectrogram test
-            Wave wave = new Wave("testData\\1.wav");
-            Spectrogram spectrogram = wave.getSpectrogram();
-            GraphicRender render = new GraphicRender();
-            System.out.println();
-            double[][] data = spectrogram.getNormalizedSpectrogramData();
-            int count = 0;
-            double max = 0;
-            double min = 1;
-            //Making sure all the lengths match up
-            int dlength = 0;
-            boolean dChanged = false;
-            boolean first = true;
-            //print all the data from data array we get back to figure out what the information means
-            for(double[] d : data){
-                if(first){
-                    dlength = d.length;
-                    first = false;
-                }
-                for(int i = 0; i < d.length; i++){
-                    System.out.print(d[i] + "--" );
-                    if(max < d[i])
-                        max = d[i];
-                    else if(min > d[i])
-                        min = d[i];
-                }
-                if(dlength < d.length){
-                    dChanged = true;
-                    dlength = d.length;
-                }
-                count++;
-                System.out.println("");
-            }
-
-            System.out.println("x = " + count);
-            System.out.println("y = " + dlength + " changed? " + dChanged);
-            System.out.println("Max: " + max + "\nMin: " + min );
-            render.setVerticalMarker(1000);
-            render.renderSpectrogramData(data, "D:\\222.jpg");
+//            //spectrogram test
+//            Wave wave = new Wave("testData\\1.wav");
+//            Spectrogram spectrogram = wave.getSpectrogram();
+//            GraphicRender render = new GraphicRender();
+//            System.out.println();
+//            double[][] data = spectrogram.getNormalizedSpectrogramData();
+//            int count = 0;
+//            double max = 0;
+//            double min = 1;
+//            //Making sure all the lengths match up
+//            int dlength = 0;
+//            boolean dChanged = false;
+//            boolean first = true;
+//            //print all the data from data array we get back to figure out what the information means
+//            for(double[] d : data){
+//                if(first){
+//                    dlength = d.length;
+//                    first = false;
+//                }
+//                for(int i = 0; i < d.length; i++){
+//                    System.out.print(d[i] + "--" );
+//                    if(max < d[i])
+//                        max = d[i];
+//                    else if(min > d[i])
+//                        min = d[i];
+//                }
+//                if(dlength < d.length){
+//                    dChanged = true;
+//                    dlength = d.length;
+//                }
+//                count++;
+//                System.out.println("");
+//            }
+//
+//            System.out.println("x = " + count);
+//            System.out.println("y = " + dlength + " changed? " + dChanged);
+//            System.out.println("Max: " + max + "\nMin: " + min );
+//            render.setVerticalMarker(1000);
+//            render.renderSpectrogramData(data, "D:\\222.jpg");
         }catch(Exception e){
             e.printStackTrace();
         }
--- Code/Spectrogram/src/Drivers/FingerPrintTest.java
+++ Code/Spectrogram/src/Drivers/FingerPrintTest.java
@@ -1,8 +1,13 @@
 package Drivers;
 
 import Wav.WaveFile;
+import com.musicg.graphic.GraphicRender;
 import fingerprint.FingerPrint;
+import fingerprint.Phrase;
+import fingerprint.Word;
 import spectrogram.Spectrogram;
+
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,10 +18,22 @@ import spectrogram.Spectrogram;
  */
 public class FingerPrintTest {
     public static void main(String[] args){
-        String fileName = "02 Canyon Wren";
+        String fileName = "480A";
         WaveFile waveFile = new WaveFile("testData\\" + fileName + ".wav");
         Spectrogram wavSpec = new Spectrogram(waveFile);
         FingerPrint fingerPrint = new FingerPrint(wavSpec);
+
+        List<Phrase> phrases = fingerPrint.getPhrases();
+
+        GraphicRender render = new GraphicRender();
+
+        int index = 0;
+        for(Phrase p : phrases){
+            for(Word w : p.getWords()){
+                render.renderSpectrogramData(w.getSpectrogram(),"testData\\out\\other\\" + fileName + index +".jpg");
+                index++;
+            }
+        }
     }
 
 }
